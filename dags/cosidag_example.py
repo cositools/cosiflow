f4a92488b87cfe56696926d9af9ca6651f1ac9b7
falric05 <falco.riccardo@outlook.it>
2025-11-13 12:25:15 +0100

    Add example DAG for COSIDAG module
    - Introduced a new example DAG, cosidag_example.py, demonstrating the usage of the COSIDAG class for automated folder monitoring and processing.
    - Implemented a custom task to process detected folders and find files matching a specific pattern using XCom.
    - Enhanced the example with configuration options for monitoring folders and task execution parameters, providing a practical reference for users.

---
+++ dags/cosidag_example.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+# add the path to the cosiflow module
+import sys
+sys.path.append("/home/gamma/airflow/modules")
+from cosidag import COSIDAG
+from airflow.operators.python import PythonOperator
+#
+def build_custom(dag):
+    # Example custom task consuming the detected folder via XCom
+    def _process_folder(folder_path: str):
+        # Do your science here
+        print(f"Processing folder: {folder_path}")
+        # search for the file by pattern
+        file_path = dag.find_file_by_pattern(r".*\.fits.*", folder_path)
+        print(f"Found file: {file_path}")
+#
+    PythonOperator(
+        task_id="custom_process",
+        python_callable=lambda ti, **_: _process_folder(
+            ti.xcom_pull(task_ids="check_new_file", key="detected_folder")
+        ),
+        dag=dag,
+    )
+#
+with COSIDAG(
+    dag_id="extended_example",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    monitoring_folders=["/home/gamma/workspace/data/tsmap"],
+    level=3,
+    date=datetime.now().strftime("%Y%m%d"),
+    build_custom=build_custom,
+    idle_seconds=5,
+    min_files=1,
+    ready_marker=None,
+    only_basename="products",
+    tags=["cosidag", "example"],
+    #ready_marker="_SUCCESS",
+) as dag:
+    pass
